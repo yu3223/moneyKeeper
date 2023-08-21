633e2fdd85c07495724f23c5ea06212e61128055
沈于庭 <jenny910223@gmail.com>
2023-08-21 16:57:47 +0800

    Give the request a response.

--- app/Controllers/MembersController.php
+++ app/Controllers/MembersController.php
@@ -76,7 +76,13 @@ class MembersController extends BaseController
         if ($memberData === null) {
             return $this->fail("Signup fail.");
         } else {
-            return redirect()->to("/signupSuccess");
+            return $this->respond([
+                "msg"  => "Signup success.",
+                "data" => [
+                    "email" => $email,
+                    "name"    => $name,
+                ]
+            ]);
         }
     }
 }
